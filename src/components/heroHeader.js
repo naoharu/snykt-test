7fe090677b8ef3a2742edd9db2ba1acab7be4a06
naoharu <naoharu@gmail.com>
2022-02-25 05:57:26 +0000

    add amazon music banner

--- src/components/heroHeader.js
+++ src/components/heroHeader.js
@@ -26,6 +26,9 @@ export default () => (
             <a href="https://podcasts.apple.com/us/podcast/jamming-fm/id1469760108?itsct=podcast_box_appicon&amp;itscg=30200" >
               <img src="/assets/logos/apple-podcast.png" alt="Apple Podcasts" height="30px"/>
             </a>　
+            <a href="https://music.amazon.com.br/podcasts/d9abb157-468d-419a-94cd-1fb3ef83b855/jamming-fm" >
+              <img src="/assets/logos/amazonmusic.png" alt="Spotify" height="30px"/>
+            </a>　
             <a href="https://open.spotify.com/show/5zCikfl5tXX9DPMQ8Hxih3" >
               <img src="/assets/logos/Spotify_Icon_RGB_Green.png" alt="Spotify" height="30px"/>
             </a>　
